8882fd14a2a9b54f6652d315b4cfcca961b85c95
marcpt98 <marcpalmat@gmail.com>
2019-06-09 20:34:25 +0200

    Revert "Start round implemented"
    This reverts commit 74943dcfbc108ff1b4ef6af2631f25447a0c1945.

--- Art_Of_Fighting/ModuleInput.h
+++ Art_Of_Fighting/ModuleInput.h
@@ -218,8 +218,6 @@ public:
 	const int JOYSTICK_DEAD_ZONE = 20000;
 	SDL_GameController* gGameController = NULL;
 	SDL_GameController* gGameController2 = NULL;
-
-	bool playerinput = false;
 };
 
 #endif // ModuleInput_H
